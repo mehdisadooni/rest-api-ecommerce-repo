dea9e5408359dcf7ae81b3bc3a83ef2464882368
Mehdi Sadooni <mdi.mehdi97@gmail.com>
2022-04-29 21:41:28 +0430

    create province Factory

---
+++ Modules/Province/Database/factories/ProvinceFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\Province\Database\factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProvinceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = \Modules\Province\Entities\Province::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->city,
+        ];
+    }
+}
+
