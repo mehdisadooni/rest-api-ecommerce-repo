883477678e98b73eeb6cc3d120e3f9e82f5e093e
Mehdi Sadooni <mdi.mehdi97@gmail.com>
2022-05-01 00:17:58 +0430

    config test

---
+++ tests/Tests/User/Http/Controllers/Api/V1/Auth/RegisterControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\Tests\User\Http\Controllers\Api\V1\Auth;
+
+use Modules\User\Http\Controllers\Api\V1\Auth\RegisterController;
+use PHPUnit\Framework\TestCase;
+
+class RegisterControllerTest extends TestCase
+{
+
+    public function testRegister()
+    {
+
+    }
+}
