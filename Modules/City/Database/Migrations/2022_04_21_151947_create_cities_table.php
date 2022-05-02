771c227ee47d1612739337fa809812da4485ab5c
Mehdi Sadooni <mdi.mehdi97@gmail.com>
2022-05-03 00:44:41 +0430

    Province Unit testing

--- Modules/City/Database/Migrations/2022_04_21_151947_create_cities_table.php
+++ Modules/City/Database/Migrations/2022_04_21_151947_create_cities_table.php
@@ -17,7 +17,7 @@ class CreateCitiesTable extends Migration
             $table->id();
 
             $table->string('name');
-            $table->foreignId('province_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('province_id')->constrained()->onDelete('cascade');
             $table->softDeletes();
 
             $table->timestamps();
---
+++ Modules/Province/Tests/Unit/Http/Controllers/Api/V1/ProvinceControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Modules\Province\Tests\Unit\Http\Controllers\Api\V1;
+
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Collection;
+use Modules\City\Entities\City;
+use Tests\TestCase;
+
+class ProvinceControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+
+    public function test_a_province_can_has_many_cities()
+    {
+        $city = City::factory()->create();
+        $province = $city->province;
+        $this->assertInstanceOf(Collection::class, $province->cities);
+        $this->assertInstanceOf(City::class, $province->cities->first());
+    }
+
+    public function test_if_province_is_force_deleted_then_all_its_cities_will_be_force_deleted()
+    {
+        $city = City::factory()->create();
+        $province = $city->province;
+
+        //create another province and city
+        $anotherCity = City::factory()->create();
+//        delete province
+        $province->forceDelete();
+        $this->assertDatabaseMissing('provinces', ['id' => $province->id]);
+        $this->assertDatabaseMissing('cities', ['id' => $city->id]);
+        $this->assertDatabaseHas('cities', ['id' => $anotherCity->id]);
+    }
+}
