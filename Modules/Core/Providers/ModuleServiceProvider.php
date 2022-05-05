cf9a3845f0fc5e72a97377309821f9f07771b1ed
Mehdi Sadooni <mdi.mehdi97@gmail.com>
2022-05-06 02:06:57 +0430

    update Core module - register all modules & route from core module

--- Modules/Core/Providers/ModuleServiceProvider.php
+++ Modules/Core/Providers/ModuleServiceProvider.php
@@ -9,34 +9,69 @@ use Illuminate\Support\ServiceProvider;
 class ModuleServiceProvider extends ServiceProvider
 {
     /**
-     * Register the service provider.
+     * Boot the application events.
      *
      * @return void
      */
-    public function register()
+    public function boot()
     {
         foreach ($this->app['modules']->allEnabled() as $module) {
-            $this->loadConfigs($module);
-            $this->loadMigrations($module);
+            $this->registerTranslations($module);
+            $this->registerConfig($module);
+//            $this->registerViews($module);
             $this->registerHelpers($module);
+            $this->loadMigrations($module);
         }
     }
 
+
+//    /**
+//     * Register views.
+//     * @param \Nwidart\Modules\Module $module
+//     * @return void
+//     */
+//    public function registerViews(Module $module)
+//    {
+//        $viewPath = resource_path('views/modules/' . $module->getLowerName());
+//
+//        $sourcePath = module_path($module->getName(), 'Resources/views');
+//
+//        $this->publishes([
+//            $sourcePath => $viewPath
+//        ], ['views',$module->getLowerName() . '-module-views']);
+//
+//        $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]),$module->getLowerName());
+//    }
+
     /**
-     * Load migrations for the given module.
-     *
+     * Register config.
      * @param \Nwidart\Modules\Module $module
      * @return void
      */
-    private function loadConfigs(Module $module)
+    protected function registerConfig(Module $module)
     {
-        collect([
-            'config' => "{$module->getPath()}/Config/config.php",
-        ])->filter(function ($path) {
-            return file_exists($path);
-        })->each(function ($path, $filename) use ($module) {
-            $this->mergeConfigFrom($path, "app.modules.{$module->getAlias()}.{$filename}");
-        });
+        $this->publishes([
+            module_path($module->getName(), 'Config/config.php') => config_path($module->getLowerName() . '.php'),
+        ], 'config');
+        $this->mergeConfigFrom(
+            module_path($module->getName(), 'Config/config.php'), $module->getLowerName()
+        );
+    }
+
+    /**
+     * Register translations.
+     * @param \Nwidart\Modules\Module $module
+     * @return void
+     */
+    public function registerTranslations(Module $module)
+    {
+        $langPath = resource_path('lang/modules/' . $module->getLowerName());
+
+        if (is_dir($langPath)) {
+            $this->loadTranslationsFrom($langPath, $module->getLowerName());
+        } else {
+            $this->loadTranslationsFrom(module_path($module->getLowerName(), 'Resources/lang'), $module->getLowerName());
+        }
     }
 
     /**
@@ -53,10 +88,10 @@ class ModuleServiceProvider extends ServiceProvider
 
     /**
      * register all php files in Support folder in all module
-     * @param $module
+     * @param \Nwidart\Modules\Module $module
      * @return void
      */
-    public function registerHelpers($module)
+    public function registerHelpers(Module $module)
     {
         foreach (glob("{$module->getPath()}/Support/*.php") as $filename) {
             require_once($filename);
--- Modules/Core/Providers/RouteServiceProvider.php
+++ Modules/Core/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Nwidart\Modules\Module;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -37,37 +38,61 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map()
     {
-        $this->mapApiRoutes();
+        $this->configureRateLimiting();
+        foreach ($this->app['modules']->allEnabled() as $module) {
+            $this->mapApiRoutes($module);
+            $this->mapWebRoutes($module);
+        }
 
-        $this->mapWebRoutes();
     }
 
     /**
      * Define the "web" routes for the application.
      *
      * These routes all receive session state, CSRF protection, etc.
-     *
+     * @param \Nwidart\Modules\Module $module
      * @return void
      */
-    protected function mapWebRoutes()
+    protected function mapWebRoutes(Module $module)
     {
+        $path = module_path($module->getName(), '/Routes/web.php');
+        if (!file_exists($path)) {
+            return;
+        }
         Route::middleware('web')
             ->namespace($this->moduleNamespace)
-            ->group(module_path('Core', '/Routes/web.php'));
+            ->group($path);
     }
 
     /**
      * Define the "api" routes for the application.
-     *
+     * @param \Nwidart\Modules\Module $module
      * These routes are typically stateless.
      *
      * @return void
      */
-    protected function mapApiRoutes()
+    protected function mapApiRoutes(Module $module)
     {
+        $path = module_path($module->getName(), '/Routes/api.php');
+        if (!file_exists($path)) {
+            return;
+        }
         Route::prefix('api')
             ->middleware('api')
-            ->namespace($this->moduleNamespace)
-            ->group(module_path('Core', '/Routes/api.php'));
+//            ->namespace($this->moduleNamespace)
+            ->group($path);
+    }
+
+
+    /**
+     * Configure the rate limiters for the application.
+     *
+     * @return void
+     */
+    protected function configureRateLimiting()
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
+        });
     }
 }
